b60b6485ad883676364f77b9fa626506bfe5e039
jaypee15 <okoyejohnpaul15@gmail.com>
2023-08-18 00:02:12 +0100

    changed to bootsrap, added more fields to models

--- dwitter/models.py
+++ dwitter/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
+from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+    joined = models.DateTimeField(default=timezone.now()) #change to auto_now_add=True,
+    location = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    website = models.CharField(max_length=255, null=True, blank=True)
+    avatar = models.ImageField(default='avatar.jpg', upload_to='profile_avatars')
+    cover_image = models.ImageField(default='', upload_to='cover_images')
+
     follows = models.ManyToManyField(
         "self",
         related_name="followed_by",
@@ -14,6 +26,14 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.avatar.path)
+        if img.size or img.width > 300:
+            img.thumbnail(300, 300)
+            img.save(self.avatar.path)
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -23,10 +43,14 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.follows.add(instance.profile)
         user_profile.save()
 
+        
+
+
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='posts')
     body = models.CharField(max_length=140)
+    post_image = models.ImageField(upload_to='post_images', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
